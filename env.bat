198985cba8871594f29a4682bcfdd0d9dc3bbcae
Gary Ho <gh395@cornell.edu>
2021-05-04 19:01:15 -0400

    instructions

--- env.bat
+++ env.bat
@@ -2,3 +2,4 @@
 SET APP_SETTINGS=config.DevelopmentConfig
 :: Set the DB url to a local database for development
 SET DATABASE_URL=postgresql://localhost/manga_recommender_db
+SET API_URL=http://localhost:5000/api/
